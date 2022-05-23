1c3abd165e91f60c69c8011bd37b3964777954ec
raselcsedev <raselcse.dev@gmail.com>
2022-05-23 09:33:52 +0600

    banner set

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
-        <div>
-            <h2>this is home</h2>
+        <div className='mx-5'>
+            <Banner></Banner>
         </div>
     );
 };
